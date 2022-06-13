e83cc1ef6e78ac0c8a1b7b93ea98449368885979
diegohcanetti <dhernandezcanetti@frba.utn.edu.ar>
2022-06-13 11:50:40 -0300

    Version2

--- Parcial Samurai Jack/src/Lib.hs
+++ Parcial Samurai Jack/src/Lib.hs
@@ -1,3 +1,32 @@
 module Lib where
 import Text.Show.Functions
 
+data Elemento = UnElemento { 
+    tipo :: String, 
+    ataque :: Efecto,
+    defensa :: Efecto
+} deriving (Show,Eq)
+
+data Personaje = UnPersonaje { 
+    nombre :: String,
+	salud :: Float,
+	elementos :: [Elemento],
+	anioPresente :: Int 
+} deriving (Show,Eq)
+
+-- modificarAno :: Int -> I
+
+type Efecto = Personaje -> Personaje
+--- Punto 01---
+
+modifcarSalud :: (Float -> Float) -> Personaje -> Personaje
+modifcarSalud funcion personaje = personaje {salud = funcion . salud  $ personaje}
+
+mandarAlAnio :: Int -> Efecto
+mandarAlAnio ano personaje = personaje {anioPresente = ano} 
+
+meditar :: Efecto
+meditar  = modifcarSalud (*1.5)
+
+causarDanio :: Float -> Efecto
+causarDanio danio = modifcarSalud (max 0 . flip (-) danio)
